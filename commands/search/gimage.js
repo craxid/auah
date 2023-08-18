a3dcdb285103f2b04efd6894fda60d6a1b07d080
Dede Kurniawan <dede.klender@gmail.com>
2023-08-18 11:02:54 +0700

    update gimage.js

--- commands/search/gimage.js
+++ commands/search/gimage.js
@@ -11,9 +11,9 @@ module.exports = {
         if (!text) return m.reply(`Example : ${prefix + command} killua Zoldyck`)
         let fetch = await fetchUrl(global.api("zenz", "/searching/gimage", { query: text }, "apikey"))
         let random = fetch.result[Math.floor(Math.random() * fetch.result.length)]
-        let buttons = [
+        /*let buttons = [
             {buttonId: `pinterest ${text}`, buttonText: { displayText: 'Next Image'}, type: 1 }
-        ]
+        ]*/
         let buttonMessage = {
             image: { url: random },
             caption: `Search Google Image Query : ${toUpper(text)}`,
