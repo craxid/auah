5753a9f21b59cbbee256a12d40fb56c52145418f
Dede Kurniawan <dede.klender@gmail.com>
2023-08-18 11:02:30 +0700

    update pin.js

--- commands/search/pin.js
+++ commands/search/pin.js
@@ -10,9 +10,9 @@ module.exports = {
     start: async(killua, m, { text, toUpper }) => {
         let fetch = await fetchUrl(global.api("zenz", "/searching/pinterest", { query: text }, "apikey"))
         let random = fetch.result[Math.floor(Math.random() * fetch.result.length)]
-        let buttons = [
+        /*let buttons = [
             {buttonId: `pin ${text}`, buttonText: { displayText: 'Next Image'}, type: 1 }
-        ]  
+        ]*/  
         let buttonMessage = {
             image: { url: random },
             caption: `Search Pinterest Query : ${toUpper(text)}`,
