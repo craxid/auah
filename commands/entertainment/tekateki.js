a017757ebaf66a11757bfe8280f7ee823dd9eab1
Dede Kurniawan <dede.klender@gmail.com>
2023-08-20 14:06:26 +0700

    update tekateki.js

--- commands/entertainment/tekateki.js
+++ commands/entertainment/tekateki.js
@@ -9,11 +9,11 @@ module.exports = {
         if (tekateki.hasOwnProperty(m.sender.split('@')[0])) return m.reply("Masih Ada Sesi Yang Belum Diselesaikan!")
         let fetch = await fetchUrl(global.api("zenz", "/entertainment/tekateki", {}, "apikey"))
         let result = await fetch.result
-        killua.sendText(m.from, `Silahkan Jawab Pertanyaan Berikut\n\n${result.soal}\n\nWaktu : 30s`, m).then(() => {
+        killua.sendText(m.from, `Silahkan Jawab Pertanyaan Berikut\n\n${result.soal}\n\nWaktu : 60 detik`, m).then(() => {
             tekateki[m.sender.split('@')[0]] = result.jawaban.toLowerCase()
             console.log("Jawaban: " + result.jawaban)
         })
-        await sleep(30000)
+        await sleep(60000)
         if (tekateki.hasOwnProperty(m.sender.split('@')[0])) {
             killua.sendText(m.from, `Waktu Habis\n\nJawaban:  ${tekateki[m.sender.split('@')[0]]}`, m)
             delete tekateki[m.sender.split('@')[0]]
