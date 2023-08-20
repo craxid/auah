dfbc8d1da00ac7edb4dee10e3c1a51732bc0b9e7
Dede Kurniawan <dede.klender@gmail.com>
2023-08-20 14:03:23 +0700

    update asahotak.js

--- commands/entertainment/asahotak.js
+++ commands/entertainment/asahotak.js
@@ -9,7 +9,7 @@ module.exports = {
         if (asahotak.hasOwnProperty(m.sender.split('@')[0])) return m.reply("Masih Ada Sesi Yang Belum Diselesaikan!")
         let fetch = await fetchUrl(global.api("zenz", "/entertainment/asahotak", {}, "apikey"))
         let result = await fetch.result
-        killua.sendText(m.from, `Silahkan Jawab Pertanyaan Berikut\n\n${result.soal}\n\nWaktu : 30s`, m).then(() => {
+        killua.sendText(m.from, `Silahkan Jawab Pertanyaan Berikut\n\n${result.soal}\n\nWaktu : 60s detik`, m).then(() => {
             asahotak[m.sender.split('@')[0]] = result.jawaban.toLowerCase()
             console.log("Jawaban: " + result.jawaban)
         })
